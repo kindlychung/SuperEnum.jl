3d6337097fa060f443f92ffcb4ebc185472341cc
Kaiyin Zhong <k.zhong@gemvision.io>
2018-09-30 14:49:19 +0200

    make the enum name more verbose and explicit

--- src/SuperEnum.jl
+++ src/SuperEnum.jl
@@ -3,9 +3,10 @@ module SuperEnum
 export @superenum
 
 macro superenum(enumname, vals...)
+    typename = Symbol(enumname, "Enum")
     expr = quote
         module $(esc(enumname))
-            Base.@enum(Enum, $(vals...))
+            Base.@enum($typename, $(vals...))
         end
     end
     expr.head = :toplevel
